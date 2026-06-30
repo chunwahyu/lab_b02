c428613ceed672a979d9a03b42c70432f0f35bf4
chunwahyu <chunwahyu@gmail.com>
2026-06-30 19:21:26 +0800

    init

---
+++ src/main/java/com/sp2603/lab_b02/PersonData.java
@@ -0,0 +1,45 @@
+package com.sp2603.lab_b02;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PersonData {
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("hkid_number")
+    private String hkid;
+
+    public PersonData(String firstName, String lastName, String hkid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hkid = hkid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getHkid() {
+        return hkid;
+    }
+
+    public void setHkid(String hkid) {
+        this.hkid = hkid;
+    }
+}
---
+++ src/test/java/com/sp2603/lab_b02/LabB02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp2603.lab_b02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabB02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
